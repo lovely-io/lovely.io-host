62fa9aa95feb95ac54b7c7b652d4fc150fe46588
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2011-06-04 10:10:10 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lovely.IO Host
+
+This project is the hosting side of the [Lovely.IO](http://lovely.io) project
+
+## Copyright & License
+
+All code in this repository is released under the terms of the MIT License
+
+Copyright (C) 2011 Nikolay Nemshilov
---
+++ config/initializers/secret_token_sample.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovelyio::Application.config.secret_token = '68ec1d83f6178ca8c734bbf186978ddcdd854fe9ab975562fc9726cc57e80e08de110d4183ff4e7a8a04abc3f3f746318d1f391719b3cb4e2704d6e3ada63474'
