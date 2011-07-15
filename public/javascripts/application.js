fe40c66697050c4b9ccb792db0a3a7b349ae3796
Nikolay Nemshilov <nemshilov@gmail.com>
2011-07-15 11:34:35 +0400

    Adding the google analytics tracker

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,3 +11,16 @@ document.addEventListener('submit', function(event) {
     }
   }
 }, false);
+
+/**
+ * GA handlers
+ */
+if (document.location.href.indexOf('lovely.io')) {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10292980-2']);
+  _gaq.push(['_trackPageview']);
+
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+}
