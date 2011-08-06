50026a1cbf4864c08532286cde86cc92445bd836
Nikolay Nemshilov <nemshilov@gmail.com>
2011-08-06 10:15:00 +0400

    The code highlighter fix for the production environment

--- lib/code_highlighter.rb
+++ lib/code_highlighter.rb
@@ -13,12 +13,10 @@ protected
   def highlight_code_blocks
     return if response.content_type != 'text/html'
 
-    response.body = response.body.gsub /([ \t]+)<pre>(.+?)<\/pre>/im do
-      code = $2.gsub "\n#{$1}", "\n"
-
-      "<pre>#{code.strip}</pre>".gsub /<pre>:([a-z]+)\s+/ do
-        "<pre data-lang=\"#{$1}\">"
-      end
+    response.body = response.body.gsub /([ \t]*)<pre>:([a-z]+)(\s+.+?)<\/pre>/m do |match|
+      %Q{<pre data-lang="#{$2}">#{ $3.gsub("\n#{$1}", "\n") }</pre>}
     end
+
+    p response.body
   end
 end
