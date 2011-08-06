a123818b542a8a792c760ff00691938f8f09b852
Nikolay Nemshilov <nemshilov@gmail.com>
2011-08-06 10:18:36 +0400

    Code highligher fix for the development environemnt

--- lib/code_highlighter.rb
+++ lib/code_highlighter.rb
@@ -13,10 +13,8 @@ protected
   def highlight_code_blocks
     return if response.content_type != 'text/html'
 
-    response.body = response.body.gsub /([ \t]*)<pre>:([a-z]+)(\s+.+?)<\/pre>/m do |match|
-      %Q{<pre data-lang="#{$2}">#{ $3.gsub("\n#{$1}", "\n") }</pre>}
+    response.body = response.body.gsub /([ \t]*)<pre>:([a-z]+)\s+(.+?)<\/pre>/m do
+      %Q{<pre data-lang="#{$2}">#{ $3.gsub("\n#{$1}", "\n").strip }</pre>}
     end
-
-    p response.body
   end
 end
