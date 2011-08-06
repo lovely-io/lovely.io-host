57c165dd16722031812d33c5a3e547b661c327ab
Nikolay Nemshilov <nemshilov@gmail.com>
2011-08-06 10:50:40 +0400

    Adding the gzipped size info to the package's sidebar

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -1,5 +1,15 @@
+require 'zlib'
+
 module PackagesHelper
   def cdn_package_url(package, version=nil)
     super package, version, :host => "cdn.#{request.host_with_port}"
+  end
+
+  def gzipped_size(string)
+    stream = Zlib::Deflate.new(Zlib::DEFAULT_COMPRESSION)
+    result = stream.deflate(string, Zlib::FINISH)
+    stream.close
+
+    number_to_human_size(result.size)
   end
 end
